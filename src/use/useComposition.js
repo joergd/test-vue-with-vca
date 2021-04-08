28fd6c4bdd034508b3f8e64c755fe45935a0094c
Joerg <joergd@pobox.com>
2021-04-08 11:12:17 +0200

    Add comments

--- src/use/useComposition.js
+++ src/use/useComposition.js
@@ -1,5 +1,12 @@
 import { ref } from "@vue/composition-api";
 
+// I need this declared outside the composition function
+// as this will act like a shared "global" for multiple
+// components that need access to this same value.
+// Some components can change this value.
+// This works flawlessly in the browser.
+// But when it comes to testing, the VCA is not yet attached
+// to the Vue instance...
 let greeting = ref("Hi there");
 
 export const useComposition = () => {
